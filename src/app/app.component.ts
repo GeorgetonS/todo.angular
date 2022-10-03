c2fd432c16f2211e980d2e239b6e89b29c1fb8e3
Georgeton <georgeton.santos.santana@gmail.com>
2022-10-03 16:25:38 -0300

    Angular
    Estudando Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  //public todos: any[] = []; // vazio
+  public todos: Todo[] = []; 
+  public title: string="Minhas Tarefas";
+  public form: FormGroup;
+  //public todos: any[]; indefinido
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+
+    });
+    this.todos.push(new Todo(1, "Estudar angular", true));
+    this.todos.push(new Todo(2, "Ir Malhar", false));
+    this.todos.push(new Todo(3, "Jogar", false));
+  }
+
+  remove(todo : Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo : Todo){
+    todo.done=true;
+  }
+
+  MarkAsUndone(todo : Todo){
+    todo.done=false;
+  }
 }
